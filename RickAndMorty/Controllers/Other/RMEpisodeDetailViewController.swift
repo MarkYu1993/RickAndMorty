5b0b530d85dc51e60d70e572370bc60723c89bfd
MarkYu <kevin60445@gmail.com>
2023-04-14 17:18:53 +0800

    Feat: Episode Cell

---
+++ RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RMEpisodeDetailViewController.swift
+//  RickAndMorty
+//
+//  Created by MarkYu on 2023/4/14.
+//
+
+import UIKit
+
+/// VC to show details about single episode
+class RMEpisodeDetailViewController: UIViewController {
+
+    private let url: URL?
+    
+    // MARK: - Init
+    
+    init(url: URL?) {
+        self.url = url
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Episode"
+    }
+}
--- RickAndMorty/Views/CharacterDetails/RMCharacterEpisodeCollectionViewCell.swift
+++ RickAndMorty/Views/CharacterDetails/RMCharacterEpisodeCollectionViewCell.swift
@@ -10,10 +10,34 @@ import UIKit
 final class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
     static let cellIdentifier = "RMCharacterEpisodeCollectionViewCell"
     
+    private let seasonLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 22, weight: .regular)
+        return label
+    }()
+    
+    private let airDateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .light)
+        return label
+    }()
+    
+    // MARK: - Init
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.backgroundColor = .systemBlue
+        contentView.backgroundColor = .tertiarySystemBackground
         contentView.layer.cornerRadius = 8
+        contentView.layer.borderWidth = 2
+        contentView.layer.borderColor = UIColor.systemBlue.cgColor
+        contentView.addSubviews(seasonLabel, nameLabel, airDateLabel)
+        setUpConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -21,16 +45,37 @@ final class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
     }
     
     private func setUpConstraints() {
+        seasonLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(10)
+            $0.trailing.equalToSuperview().offset(-10)
+            $0.height.equalToSuperview().multipliedBy(0.3)
+        }
         
+        nameLabel.snp.makeConstraints {
+            $0.top.equalTo(seasonLabel.snp.bottom)
+            $0.leading.trailing.height.equalTo(seasonLabel)
+        }
+        
+        airDateLabel.snp.makeConstraints {
+            $0.top.equalTo(nameLabel.snp.bottom)
+            $0.leading.trailing.height.equalTo(seasonLabel)
+        }
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        seasonLabel.text = nil
+        nameLabel.text = nil
+        airDateLabel.text = nil
     }
     
     public func configure(with viewModel: RMCharacterEpisodeCollectionViewCellViewModel) {
-        viewModel.registerForData { data in
-            print("data:\(data)")
+        viewModel.registerForData { [weak self] data in
+            // Main Queue
+            self?.nameLabel.text = data.name
+            self?.seasonLabel.text = "Episode \(data.episode)"
+            self?.airDateLabel.text = "Aired on \(data.airDate)"
         }
         viewModel.fetchEpisode()
     }
