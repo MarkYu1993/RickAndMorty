87d29df4c73015db73e06517626db4de09511b09
MarkYu <kevin60445@gmail.com>
2023-04-21 17:14:08 +0800

    Feat: Setting ViewModels

---
+++ RickAndMorty/ViewModels/Settings/RMSettingsViewViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RMSettingsViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by MarkYu on 2023/4/21.
+//
+
+import Foundation
+
+struct RMSettingsViewViewModel {
+    let cellViewModels: [RMSettingsCellViewModel]
+}
