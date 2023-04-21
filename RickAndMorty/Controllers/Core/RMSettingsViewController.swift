7b5f1cf7716225c28739c5a17863947c83108db3
MarkYu <kevin60445@gmail.com>
2023-04-21 17:47:23 +0800

    ***Feat: SwiftUI Settings View***

--- RickAndMorty/Controllers/Core/RMSettingsViewController.swift
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -6,19 +6,37 @@
 //
 
 import UIKit
+import SwiftUI
 
 /// Controller to show various app options and settings
 final class RMSettingsViewController: UIViewController {
-
+    
     private let viewModel = RMSettingsViewViewModel(
         cellViewModels: RMSettingsOption.allCases.compactMap({
         return RMSettingsCellViewModel(type: $0)
     }))
     
+    private let settingsSwiftUIController =
+    UIHostingController(rootView: RMSettingsView(
+        viewModel: RMSettingsViewViewModel(
+            cellViewModels: RMSettingsOption.allCases.compactMap({
+                return RMSettingsCellViewModel(type: $0)
+            }))))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         self.title = "Settings"
+        addSwiftUIController()
     }
  
+    private func addSwiftUIController() {
+        addChild(settingsSwiftUIController)
+        settingsSwiftUIController.didMove(toParent: self)
+        view.addSubview(settingsSwiftUIController.view)
+        
+        settingsSwiftUIController.view.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
 }
