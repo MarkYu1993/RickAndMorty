8a40a9ab52011f3eba9f345b9e56984dd8608f54
MarkYu <kevin60445@gmail.com>
2023-04-25 15:18:31 +0800

    Feat: Tap Setting Options

--- RickAndMorty/Controllers/Core/RMSettingsViewController.swift
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -7,21 +7,11 @@
 
 import UIKit
 import SwiftUI
+import SafariServices
 
 /// Controller to show various app options and settings
 final class RMSettingsViewController: UIViewController {
-    
-    private let viewModel = RMSettingsViewViewModel(
-        cellViewModels: RMSettingsOption.allCases.compactMap({
-        return RMSettingsCellViewModel(type: $0)
-    }))
-    
-    private let settingsSwiftUIController =
-    UIHostingController(rootView: RMSettingsView(
-        viewModel: RMSettingsViewViewModel(
-            cellViewModels: RMSettingsOption.allCases.compactMap({
-                return RMSettingsCellViewModel(type: $0)
-            }))))
+    private var settingsSwiftUIController: UIHostingController<RMSettingsView>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,12 +21,36 @@ final class RMSettingsViewController: UIViewController {
     }
  
     private func addSwiftUIController() {
+        let settingsSwiftUIController =
+        UIHostingController(rootView: RMSettingsView(
+            viewModel: RMSettingsViewViewModel(
+                cellViewModels: RMSettingsOption.allCases.compactMap({
+                    return RMSettingsCellViewModel(type: $0) { [weak self]option in
+                        self?.handleTap(option: option)
+                    }
+                }))))
         addChild(settingsSwiftUIController)
         settingsSwiftUIController.didMove(toParent: self)
+        
         view.addSubview(settingsSwiftUIController.view)
         
         settingsSwiftUIController.view.snp.makeConstraints {
             $0.top.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        self.settingsSwiftUIController = settingsSwiftUIController
+    }
+    
+    private func handleTap(option: RMSettingsOption) {
+        guard Thread.current.isMainThread else {
+            return
+        }
+        if let url = option.targetUrl {
+            // Open Website
+            let vc = SFSafariViewController(url: url)
+            present(vc, animated: true)
+        } else if option == .rateApp {
+            // show rating prompt
         }
     }
 }
--- RickAndMorty/ViewModels/Settings/RMSettingsOption.swift
+++ RickAndMorty/ViewModels/Settings/RMSettingsOption.swift
@@ -16,6 +16,25 @@ enum RMSettingsOption: CaseIterable {
     case viewSeries
     case viewCode
     
+    var targetUrl: URL? {
+        switch self {
+        case .rateApp:
+            return nil
+        case .contactUs:
+            return URL(string: "https://tw.yahoo.com")
+        case .terms:
+            return URL(string: "https://youtube.com")
+        case .privacy:
+            return URL(string: "https://google.com")
+        case .apiReference:
+            return URL(string: "https://ck101.tw/")
+        case .viewSeries:
+            return URL(string: "https://www.youtube.com/watch?v=JIe33OKk7Jg")
+        case .viewCode:
+            return URL(string: "https://github.com/AfrazCodes/RickAndMortyiOSApp")
+        }
+    }
+    
     var displayTitle: String {
         switch self {
         case .rateApp:
